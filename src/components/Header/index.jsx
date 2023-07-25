d21f74fb754b47efed2a50366a5735578daf7536
Ibikivan <ibikivan1@gmail.com>
2023-07-25 20:28:50 +0200

    Nav bar added

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,28 +1,46 @@
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import colors from '../../utils/style/colors';
+import lightLogo from '../../assets/dark-logo.png';
+
+const StyledNav = styled.nav`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    margin: 40px;
+`
 
 const StyledLink = styled(Link)`
-    padding: 15px;
+    padding: 11px 30px;
     color: ${colors.secondary};
     text-decoration: none;
-    font-size: 18px
     ${(props) =>
         props.$isFullLink && `
             color: white;
-            border-raduis: 30px;
+            border-radius: 30px;
             background-color: ${colors.primary};
         `
     }
 `
 
+const StyledLogo = styled.img`
+    width: 187px;
+    height: 70px;
+    flex-shrink: 0;
+`
+
 function Header() {
     return (
-        <nav>
-            <StyledLink to="/">Accueil</StyledLink>
-            <StyledLink to="/survey/1" $isFullLink>Questionnaire</StyledLink>
-            <StyledLink to="/freelances">Freelances</StyledLink>
-        </nav>
+        <StyledNav>
+            <StyledLogo src={lightLogo} alt="shiny logo" />
+
+            <div>
+                <StyledLink to="/">Accueil</StyledLink>
+                <StyledLink to="/survey/1" $isFullLink>Questionnaire</StyledLink>
+                <StyledLink to="/freelances">Freelances</StyledLink>
+            </div>
+        </StyledNav>
     )
 }
 
