81484cc732a490ba4e58622c45f8ade56d7adf2c
Ibikivan <ibikivan1@gmail.com>
2023-07-25 23:07:59 +0200

    Error page added

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -1,8 +1,30 @@
+import styled from 'styled-components';
+import back404 from '../../assets/404.svg';
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background: #F9F9FC; 
+    margin: 90px;
+    padding: 100px;
+    gap: 50px;
+`
+
+const StyledP = styled.p`
+    font-size: 31px;
+    font-weight: 700;
+    line-height: normal;
+`
+
 function Error() {
     return (
-        <div>
-            <h1>Oups 🙈 Cette page n'existe pas</h1>
-        </div>
+        <Container>
+            <h1>Oups...</h1>
+            <img src={back404} alt="Error page background" />
+            <StyledP>Il semblerait qu’il y ait un problème</StyledP>
+        </Container>
     )
 }
 
